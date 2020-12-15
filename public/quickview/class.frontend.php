65a28977a7e0d75f469bbfa5e9d96abf18ac4891
xoinstein <xoinstein@gmail.com>
2020-12-15 17:46:01 +0800

    update

--- public/quickview/class.frontend.php
+++ public/quickview/class.frontend.php
@@ -138,8 +138,7 @@ class pisol_quick_view_frontend{
     public function pisol_add_button2($add_to_cart_url){
 
 		global $post;
-        return '<div class="product-action"><a data-product-id="'.$post->ID.'"class="quick_view button pisol_quick_view_button" >
-        <span>'.$this->pi_dcw_quick_view_text.'</span></a>' . $add_to_cart_url . '</div>';
+        return '<div class="product-action"><a data-product-id="'.$post->ID.'"class="quick_view button pisol_quick_view_button" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg><span>'.$this->pi_dcw_quick_view_text.'</span></a>' . $add_to_cart_url . '</div>';
     }
     
 	public function pisol_add_button(){
